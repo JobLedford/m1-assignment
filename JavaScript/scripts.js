1a4cb189cbd1d7e1b0fa38400a061d7c8a2227bc
JobLedford <j.ledford.421@westcliff.edu>
2023-02-05 21:00:45 -0500

    added some JavaScript

---
+++ JavaScript/scripts.js
@@ -0,0 +1,19 @@
+var image = [
+  "/images/cat.jpg",
+  "/images/cat2,jpg",
+  "/images/cat3,jpg",
+  "/images/dog1.jpg",
+  "/images/dog2,jpg",
+  "/images/doge.jpg",
+  "/images/doge2.jpg",
+  "/images/doge3.jpg",
+  "/images/doge4.jpg",
+  "/images/doghelpers.jpg",
+  "/images/doghelper2.jpg",
+  "/images/snek.jpg",
+  "/images/snek2.jpg",
+];
+
+for (var i = 0; i < 13; i++) {
+  console.log(image[i]);
+}
